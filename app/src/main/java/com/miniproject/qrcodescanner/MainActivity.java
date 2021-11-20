46cf0b45d884d843479276df5d5b3a31f298dbcf
starlight5234 <starlight5234@protonmail.ch>
2021-11-20 23:55:47 +0530

    Intial QR App code
    - Provides option to copy the result
    
    Change-Id: I05061973aa3752b00016e174786ea4eed3294195
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- app/src/main/java/com/miniproject/qrcodescanner/MainActivity.java
+++ app/src/main/java/com/miniproject/qrcodescanner/MainActivity.java
@@ -1,14 +1,84 @@
 package com.miniproject.qrcodescanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.text.ClipboardManager;
+
 
 public class MainActivity extends AppCompatActivity {
+    // Create a dialog box
+    protected Dialog dialogBox;
+
+    @SuppressLint("StaticFieldLeak")
+    protected static TextView scanResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scanResult = (TextView)findViewById(R.id.scanResult);
+        // Set margins of scanResult programmatically
+        ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT);
+        params.setMargins(30,200,30,325);
+        scanResult.setLayoutParams(params);
+
+        // Create a dialog box to copy QR Data
+        dialogBox = new Dialog(MainActivity.this);
+        dialogBox.setContentView(R.layout.result_dialog);
+        dialogBox.getWindow().setBackgroundDrawable(getDrawable(R.drawable.background));
+        dialogBox.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialogBox.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+
+        scanResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogBox.show();
+            }
+        });
+
+        // Scan button which opens launches QR Scanner class
+        Button scanButton = this.findViewById(R.id.scanButton);
+
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),scannerView.class));
+            }
+        });
+
+        // Dialog functions and buttons
+        Button Okay = dialogBox.findViewById(R.id.buttonOkay);
+        Button Copy = dialogBox.findViewById(R.id.buttonCopy);
+
+        Okay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogBox.dismiss();
+            }
+        });
+
+        Copy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClipboardManager cm = (ClipboardManager)getApplicationContext().getSystemService(Context.CLIPBOARD_SERVICE);
+                cm.setText(scanResult.getText().toString());
+                Toast.makeText(MainActivity.this, "Copied to clipboard!", Toast.LENGTH_SHORT).show();
+                dialogBox.dismiss();
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/miniproject/qrcodescanner/scannerView.java
@@ -0,0 +1,69 @@
+package com.miniproject.qrcodescanner;
+
+import android.Manifest;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class scannerView extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    ZXingScannerView scannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner_view);
+
+        scannerView=new ZXingScannerView(this);
+        setContentView(scannerView);
+
+        Dexter.withContext(getApplicationContext())
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        scannerView.startCamera();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                        MainActivity.scanResult.setText(R.string.camera_denied);
+                        onBackPressed();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        MainActivity.scanResult.setText(rawResult.getText());
+        onBackPressed();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scannerView.stopCamera();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        scannerView.setResultHandler(this);
+        scannerView.startCamera();
+    }
+}
